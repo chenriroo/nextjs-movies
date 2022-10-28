893760ce58c0e1bd5f92aa2fd3613ecc75836c3e
chenriroo <chenriroo@gmail.com>
2022-10-28 13:38:11 +0200

    changed firebase admin-sdk serviceaccount data to single variable

--- src/lib/firebaseAdmin.js
+++ src/lib/firebaseAdmin.js
@@ -1,13 +1,16 @@
 import * as admin from 'firebase-admin'
 
 if(!admin.apps.length) {
-	admin.initializeApp({
-		credential: admin.credential.cert({
-			projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-			clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-			privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-		})
-	});
+	try {
+		const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK)
+
+		admin.initializeApp({
+			credential: admin.credential.cert(serviceAccount)
+		});
+	} catch (error) {
+		console.log('Firebase admin initionalization error', error.stack)
+	}
+
 
 }
 
